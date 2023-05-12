d3454c09a95657a979baeb36fb701e2eeeb21a17
Miqstelles <90120169+Miqstelles@users.noreply.github.com>
2023-05-12 20:00:00 -0300

    initial commit: learning svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<h1>Layout</h1>
+
+<ul>
+    <li><a href="/">Home</a></li>
+    <li><a href="/sobre">Sobre</a></li>
+    <li><a href="/portifolio">Portifolio</a></li>
+    <li><a href="/contato">Contato</a></li>
+</ul>
+
+<slot />
